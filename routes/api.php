df1142c80b55951396a862324a401460d63c35b2
ronnysantana <m@rsantana.us>
2026-01-02 09:41:52 -0300

    Update currency value in API route

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Number;
 
 Route::get('/', function (Request $request) {
-    return ['msg' => 'oii', 'value' => Number::currency(1000000009, in: 'BRL', locale: 'pt_BR')];
+    return ['msg' => 'oii', 'value' => Number::currency(1091000000, in: 'BRL', locale: 'pt_BR')];
 });
