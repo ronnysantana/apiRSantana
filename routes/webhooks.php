60bbd110f4e92ebc31de5a96a96201d1830f67a2
ronnysantana <m@rsantana.us>
2026-01-02 15:08:10 -0300

    webhooks response

--- routes/webhooks.php
+++ routes/webhooks.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Number;
 
 Route::get('/', function (Request $request) {
-    return "Oiii";
+    return response(['route' => 'm9', 'value' => Number::currency(500.000.000, in: 'BRL', locale: 'pt_BR')]);
 });
