f9cdf8e63c6cc4826efc6043e55cdaca4156c6b5
ronnysantana <m@rsantana.us>
2026-01-02 15:05:13 -0300

    webhooks

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+        Route::middleware('api')
+            ->prefix('webhooks')
+            ->name('webhooks.')
+            ->group(base_path('routes/webhooks.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
