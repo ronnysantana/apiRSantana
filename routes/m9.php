e4ddf62dc53144de29693895717b197e28d28709
ronnysantana <m@rsantana.us>
2026-01-02 15:14:37 -0300

    ,,,

--- routes/m9.php
+++ routes/m9.php
@@ -5,8 +5,5 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Number;
 
 Route::get('/', function (Request $request) {
-    return response([
-        'route' => 'm9',
-        'value' => Number::currency(500.000.000, in: 'BRL', locale: 'pt_BR'),
-    ]);
+    return ['msg' => 'oii', 'value' => Number::currency(1091000500, in: 'BRL', locale: 'pt_BR')];
 });
